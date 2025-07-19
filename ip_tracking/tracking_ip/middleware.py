f12b5a88c7faabcd920646f688c0ade959ccd9e1
baydre_africa <baydreafrica@gmail.com>
2025-07-19 17:05:43 +0100

    chore: Basic IP Logging Middleware Implementation

---
+++ ip_tracking/tracking_ip/middleware.py
@@ -0,0 +1,28 @@
+from tracking_ip.models import RequestLog
+from django.utils.deprecation import MiddlewareMixin
+from django.http import HttpResponseForbidden
+import logging
+
+logger = logging.getLogger(__name__)
+
+class BasicIPLoggingMiddleware(MiddlewareMixin):
+    """
+    Middleware to log the IP address, timestamp, and path of every incoming request.
+    """
+    def process_request(self, request):
+        """
+        Process the request to log IP details.
+        """
+        ip_address = request.META.get('REMOTE_ADDR')
+
+        if ip_address:
+            path = request.path
+            try:
+                RequestLog.objects.create(
+                    ip_address=ip_address,
+                    path=path
+                )
+                # logger.info(f"Logged request: IP={ip_address}, Path={path}")
+            except Exception as e:
+                logger.error(f"Error logging request: {e}", exc_info=True)
+        return None # Must return None or an HttpResponse object
